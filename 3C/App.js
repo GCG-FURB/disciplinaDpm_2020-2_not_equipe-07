262a17245dbb05314b3e140d5d9a7acdf14e6076
matheus-soares-lima <matheus.lima@paytrack.com.br>
2020-09-29 21:11:43 -0300

    Hello world

---
+++ 3C/App.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "lightblue",
+      }}
+    >
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
