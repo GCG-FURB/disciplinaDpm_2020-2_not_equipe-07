0c24396ac491aaf3eadaea3cf739fbf701c0b900
matheus-soares-lima <matheus.lima@paytrack.com.br>
2020-10-26 23:15:28 -0300

    Inicio 3H

---
+++ Unidade/3H/src/entidades.ts
@@ -0,0 +1,14 @@
+class Pessoa {
+  public nome: string;
+  public idade: number;
+  public endereco: Endereco;
+}
+
+class Endereco {
+  public rua: string;
+  public cidade: string;
+  public estado: string;
+  public cep: string;
+  public latitude: string;
+  public longitude: string;
+}
